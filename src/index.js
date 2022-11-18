f126ad5fa3d20908e55e94ea31a0571f38d04e4a
dorigoluiz <luizdorigo01@gmail.com>
2022-11-18 15:03:29 -0300

    feat: index routes && mongodb

--- src/index.js
+++ src/index.js
@@ -6,6 +6,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.post("/sign-in", () => {});
+
+app.post("/sign-up", () => {});
+
+app.get("/home", () => {});
+
+app.post("/new-entry", () => {});
+
+app.post("/new-exit", () => {});
+
 app.listen(5000, () => {
     console.log(chalk.bold.cyan("[Listening ON] Port: 5000."));
 });
